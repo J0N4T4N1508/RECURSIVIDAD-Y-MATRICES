0dc0ba4a777f029cd4041a2528e54e4192be1a71
J0N4T4N1508 <167550570+J0N4T4N1508@users.noreply.github.com>
2024-05-10 20:42:39 -0500

    Add files via upload

---
+++ EJERCICIO-03/Program.cs
@@ -0,0 +1,32 @@
+﻿// Ejercicio 3: Escribe una función recursiva que imprima la pirámide de números del 1 al n. 
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ImprimirNúmeroPirámide(5); // Puedes cambiar el número aquí
+    }
+
+    static void ImprimirNúmeroPirámide(int n, int filaActual = 1)
+    {
+        if (filaActual > n)
+            return;
+
+        ImprimirFila(n, filaActual);
+        Console.WriteLine();
+
+        ImprimirNúmeroPirámide(n, filaActual + 1);
+    }
+
+    static void ImprimirFila(int n, int filaActual)
+    {
+        if (filaActual == 0)
+            return;
+
+        ImprimirFila(n, filaActual - 1);
+        Console.Write(filaActual + " ");
+    }
+}
+
