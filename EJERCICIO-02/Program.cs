859e54bd17417ec8388f1ab4eb1e7daa8a691a35
J0N4T4N1508 <167550570+J0N4T4N1508@users.noreply.github.com>
2024-05-10 20:40:24 -0500

    Add files via upload

---
+++ EJERCICIO-02/Program.cs
@@ -0,0 +1,27 @@
+﻿// Ejercicio 2: Escribe una función recursiva que imprima la suma de los números del 1 al n.
+
+using System.Runtime.InteropServices.ObjectiveC;
+
+class program 
+{
+    static void Main(string[] args)
+    {
+        int n = 5;
+        Console.WriteLine($"La suma de los numeros del 1 al {n} son:");
+        int resultado = ImprimirSuma(5);
+        Console.WriteLine(resultado);
+    }
+
+    static int ImprimirSuma(int n) 
+    {
+        if(n == 1) 
+        {
+            return 1;
+        }
+        else 
+        {
+            return n + ImprimirSuma(n - 1);  
+        }
+    }
+}
+
