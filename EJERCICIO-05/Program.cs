9522330daefb28f3d56d15f432c23c436964e051
J0N4T4N1508 <167550570+J0N4T4N1508@users.noreply.github.com>
2024-05-10 20:48:25 -0500

    Add files via upload

---
+++ EJERCICIO-05/Program.cs
@@ -0,0 +1,27 @@
+﻿// Escribe una función recursiva que imprima la tabla de multiplicar del n. 
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 5; // Puedes cambiar el número aquí
+        ImprimirTablaMultiplicar(n);
+    }
+
+    static void ImprimirTablaMultiplicar(int n, int multiplicador = 1)
+    {
+        if (multiplicador > 12) // Imprimimos hasta 12 multiplicaciones
+            return;
+
+        Console.WriteLine($"{n} x {multiplicador} = {n * multiplicador}");
+
+        ImprimirTablaMultiplicar(n, multiplicador + 1);
+    }
+}
+
+
+
+
+
