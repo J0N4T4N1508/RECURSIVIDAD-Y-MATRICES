25ce863332ba3069ff804e37b3303f4c650d7e39
J0N4T4N1508 <167550570+J0N4T4N1508@users.noreply.github.com>
2024-05-10 20:34:43 -0500

    Add files via upload

---
+++ EJERCICIO-01/Program.cs
@@ -0,0 +1,26 @@
+﻿// Ejercicio 1: Escribe una función recursiva que imprima los números pares del 1 al 100. 
+
+
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Los números pares del 1 al 100 son:");
+        ImprimirPares(1);
+    }
+
+    static void ImprimirPares(int numero)
+    {
+        if (numero <= 100)
+        {
+            if (numero % 2 == 0)
+            {
+                Console.Write(numero + " ");
+            }
+            ImprimirPares(numero + 1); // Llamada recursiva para el siguiente número
+        }
+    }
+}
