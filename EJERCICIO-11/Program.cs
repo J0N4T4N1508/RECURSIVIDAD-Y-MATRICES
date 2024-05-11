e4caa5e639c73600947167c6c2711232438aae50
J0N4T4N1508 <167550570+J0N4T4N1508@users.noreply.github.com>
2024-05-10 22:11:35 -0500

    Add files via upload

---
+++ EJERCICIO-11/Program.cs
@@ -0,0 +1,60 @@
+﻿// Multiplica una matriz por un número. 
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Definir una matriz
+        int[,] matriz = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        // Definir el número por el cual multiplicar la matriz
+        int multiplicador = 2;
+
+        // Multiplicar la matriz por el multiplicador
+        int[,] matrizResultado = MultiplicarMatrizPorNumero(matriz, multiplicador);
+
+        // Imprimir la matriz resultante
+        ImprimirMatriz(matrizResultado);
+    }
+
+    static int[,] MultiplicarMatrizPorNumero(int[,] matriz, int numero)
+    {
+        int filas = matriz.GetLength(0);
+        int columnas = matriz.GetLength(1);
+        int[,] matrizResultado = new int[filas, columnas];
+
+        // Multiplicar cada elemento de la matriz por el número
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                matrizResultado[i, j] = matriz[i, j] * numero;
+            }
+        }
+
+        return matrizResultado;
+    }
+
+    static void ImprimirMatriz(int[,] matriz)
+    {
+        int filas = matriz.GetLength(0);
+        int columnas = matriz.GetLength(1);
+
+        Console.WriteLine("Matriz Resultado:");
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                Console.Write(matriz[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
