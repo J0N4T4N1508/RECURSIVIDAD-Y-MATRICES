57a82d31c52822c6c7bd68f38be830364e780763
J0N4T4N1508 <167550570+J0N4T4N1508@users.noreply.github.com>
2024-05-10 22:18:12 -0500

    Add files via upload

---
+++ EJERCICIO-15/Program.cs
@@ -0,0 +1,39 @@
+﻿// Escribe una función que encuentre el elemento máximo de una matriz. 
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Definir una matriz de ejemplo
+        int[,] matriz = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        // Encontrar el elemento máximo
+        int maximo = EncontrarMaximoMatriz(matriz);
+
+        // Imprimir el resultado
+        Console.WriteLine("El elemento máximo de la matriz es: " + maximo);
+    }
+
+    static int EncontrarMaximoMatriz(int[,] matriz)
+    {
+        int maximo = matriz[0, 0]; // Asumimos que el primer elemento es el máximo inicialmente
+
+        // Recorrer la matriz para encontrar el máximo
+        foreach (int elemento in matriz)
+        {
+            if (elemento > maximo)
+            {
+                maximo = elemento;
+            }
+        }
+
+        return maximo;
+    }
+}
+
