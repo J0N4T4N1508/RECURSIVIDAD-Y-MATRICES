6e8f36c0e1d40f514e535eb833cb95c9d538dc22
J0N4T4N1508 <167550570+J0N4T4N1508@users.noreply.github.com>
2024-05-10 20:45:35 -0500

    Add files via upload

---
+++ EJERCICIO-04/Program.cs
@@ -0,0 +1,32 @@
+﻿// Escribe una función recursiva que imprima la pirámide de números invertidos del 1 al n. 
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ImprimirPiramideNumerosInvertida(5); // Puedes cambiar el número aquí
+    }
+
+    static void ImprimirPiramideNumerosInvertida(int n, int filaActual = 1)
+    {
+        if (filaActual > n)
+            return;
+
+        ImprimirFilaInvertida(n, filaActual);
+        Console.WriteLine();
+
+        ImprimirPiramideNumerosInvertida(n, filaActual + 1);
+    }
+
+    static void ImprimirFilaInvertida(int n, int filaActual)
+    {
+        if (filaActual == 0)
+            return;
+
+        Console.Write(filaActual + " ");
+        ImprimirFilaInvertida(n, filaActual - 1);
+    }
+}
+
